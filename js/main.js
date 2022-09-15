c651d1f8ed950ef0f5a91f47e82bef3d584d25d4
Adam Crowley <adamcrowley@gmail.com>
2022-09-15 17:53:31 +1200

    fix layout bugs

--- js/main.js
+++ js/main.js
@@ -1,45 +1,4 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-//About section: Text animation
-let aboutTl = gsap.timeline()
-.fromTo('.section--about__name span:not(.sr-only)', { y: 15 }, { duration: 1, autoAlpha: 1, y: 0, ease: Power2.easeOut, stagger: 0.075 })
-.to('.section--about__title', { duration: 1, autoAlpha: 1, ease: Power2.easeOut }, '-=0.75')
-.fromTo('.section--about__border', { width: 0 }, { duration: 1.25, autoAlpha: 1, width: '100%', ease: Power2.easeOut }, '-=1')
-.fromTo('.section--about__p', { y: 15 }, { duration: 1, autoAlpha: 1, y: 0, ease: Power2.easeOut, stagger: 0.15 }, '-=0.75');
-
-//About section: parallax effect
-gsap.to('.section--about__text-container', {
-	yPercent: 50,
-	ease: "none",
-	scrollTrigger: {
-		trigger: '.section--about',
-		markers: false,
-		start: "top 0%",
-		scrub: true
-	}
-})
-
-//Work section: header tween animation
-let workTl = gsap.timeline({scrollTrigger: {
-	trigger: '.section--about',
-	start: "top 0%"
-}})
-.fromTo('.section--work__header span', { y: 15 }, { duration: 1, autoAlpha: 1, y: 0, ease: Power2.easeOut, stagger: 0.075 })
-.fromTo('.section--work__border', { width: 0 }, { duration: 1.25, autoAlpha: 1, width: '100%', ease: Power2.easeOut }, '-=1')
-
-//Work section: fade in text elements
-const fadeInElements = gsap.utils.toArray('.section--work__img-container, .section--work__h3, .section--work__title, .section--work__list, .section--work__a');
-fadeInElements.forEach(elem => {
-	gsap.from(elem, {
-		duration: 1,
-		y: 15,
-		autoAlpha: 0,
-		scrollTrigger: {
-			trigger: elem,
-			start: "top 90%"
-    	}
-  	})
-});
   
 //navigation
 const body = document.querySelector('body');
@@ -96,3 +55,48 @@ function handleTabletChange(e) {
 mediaQuery.addEventListener('change', handleTabletChange);
 handleTabletChange(mediaQuery);
 
+//About section: Text animation
+let aboutTl = gsap.timeline()
+.fromTo('.section--about__name span:not(.sr-only)', { y: 15 }, { duration: 1, autoAlpha: 1, y: 0, ease: Power2.easeOut, stagger: 0.075 })
+.to('.section--about__title', { duration: 1, autoAlpha: 1, ease: Power2.easeOut }, '-=0.75')
+.fromTo('.section--about__border', { width: 0 }, { duration: 1.25, autoAlpha: 1, width: '100%', ease: Power2.easeOut }, '-=1')
+.fromTo('.section--about__p', { y: 15 }, { duration: 1, autoAlpha: 1, y: 0, ease: Power2.easeOut, stagger: 0.15 }, '-=0.75');
+
+//About section: parallax effect
+let mm = gsap.matchMedia();
+mm.add("(min-width: 768px)", () => {
+
+	gsap.to('.section--about__text-container', {
+		yPercent: 50,
+		ease: "none",
+		scrollTrigger: {
+			trigger: '.section--about',
+			markers: false,
+			start: "top 0%",
+			scrub: true
+		}
+	})
+
+});
+
+//Work section: header tween animation
+let workTl = gsap.timeline({scrollTrigger: {
+	trigger: '.section--about',
+	start: "top 0%"
+}})
+.fromTo('.section--work__header span', { y: 15 }, { duration: 1, autoAlpha: 1, y: 0, ease: Power2.easeOut, stagger: 0.075 })
+.fromTo('.section--work__border', { width: 0 }, { duration: 1.25, autoAlpha: 1, width: '100%', ease: Power2.easeOut }, '-=1')
+
+//Work section: fade in text elements
+const fadeInElements = gsap.utils.toArray('.section--work__img-container, .section--work__h3, .section--work__title, .section--work__list, .section--work__a');
+fadeInElements.forEach(elem => {
+	gsap.from(elem, {
+		duration: 1,
+		y: 15,
+		autoAlpha: 0,
+		scrollTrigger: {
+			trigger: elem,
+			start: "top 90%"
+    	}
+  	})
+});
